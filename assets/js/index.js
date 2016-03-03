0e09e2e3dde8ebdc993ba4fd19df759a71364d87
Ian Damen <ian.damen@digital@homeoffice.gov.uk>
2016-03-03 11:57:56 +0000

    Provide expand/collapse option on appeal stage reference list

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,26 +10,27 @@ var $ = require('jquery');
 helpers.documentReady(progressiveReveal);
 helpers.documentReady(formFocus);
 
+if ($('.appeal-summary-class').length) {
+  $('.appeal-summary-class').click(function() {
+    $('#' + this.title).toggleClass('hidden');
+  });
+}
+
+
 if ($('#which-england-and-wales').length) {
   $('#which-england-and-wales').click(function() {
-
     $('#england-and-wales-dates').toggleClass('hidden');
-
   });
 }
 
 if ($('#which-scotland').length) {
   $('#which-scotland').click(function() {
-
     $('#scotland-dates').toggleClass('hidden');
-
   });
 }
 
 if ($('#which-northern-ireland').length) {
   $('#which-northern-ireland').click(function() {
-
     $('#northern-ireland-dates').toggleClass('hidden');
-
   });
 }
