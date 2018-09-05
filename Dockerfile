ce2039e543216dd5d9531c7f0d48270308a43095
Ben Marvell <madmarv187@gmail.com>
2018-09-05 15:36:20 +0100

    Remove kube files
    Migrate to LTS node 8
    
    Add drone config

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM quay.io/ukhomeofficedigital/nodejs:v1.0.1
+FROM quay.io/ukhomeofficedigital/nodejs-base:v8
 
-CMD ["npm", "run", "dev"]
+RUN mkdir /public
+
+COPY package.json /app/package.json
+RUN npm --loglevel warn install --production --no-optional
+COPY . /app
+RUN npm --loglevel warn run postinstall
+RUN chown -R nodejs:nodejs /public
+
+USER 999
+
+CMD ["/app/run.sh"]
--- run.sh
+++ run.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
+cp -r /app/public/* /public/
 
-if [ "$NODE_ENV" = "development" ] #use this for local machine dev on port 8080
-then echo "starting the service"
-     if pidof -o %PPID -x "are">/dev/null; then
-       echo "stopping pid $$"
-       npm stop
-     fi
-
-     CONFIG_FILE=config_dev.yml npm run dev
-
-
-elif [ "$NODE_ENV" = "so-ci" ] #use this on ci.so
-then echo "starting service"
-     SITEROOT=/are node /var/www/are/app.js
-fi
+node app.js
