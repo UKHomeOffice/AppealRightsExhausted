88fa60a0f7491eb3683daa5914c476b9beaf822f
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2021-06-28 20:14:19 +0100

    SD-993: Upgrade linter to use eslint-config-hof@1.0.4

--- test/unit/apps/are_form/lib/bulkdatetest.js
+++ test/unit/apps/are_form/lib/bulkdatetest.js
@@ -4,41 +4,38 @@
 
 'use strict';
 
-var moment = require('moment');
+const moment = require('moment');
 
 process.env.NODE_ENV = 'test';
-var assert = require('assert');
-var myStages = require('../../../../../apps/are_form/lib/staticAppealStages.js').getstaticAppealStages();
-var are = require('../../../../../apps/are_form/lib/classARE.js');
+const assert = require('assert');
+const myStages = require('../../../../../apps/are_form/lib/staticAppealStages.js').getstaticAppealStages();
+const are = require('../../../../../apps/are_form/lib/classARE.js');
 
 // ------
 // for each appeal stage, and for each country, this test will check
 // up to a determined max amount of consecutive dates to ensure that
 // the resultant ARE date is NOT either a weekend nor an Exclusion date
 
-var maxDatesToTest = 100;
+const maxDatesToTest = 100;
 
-describe('Bulk Test: checking ARE is not weekend nor exclusion day', function() {
+describe('Bulk Test: checking ARE is not weekend nor exclusion day', function () {
+  let i = 0;
+  let d = null;
+  const testDate = moment('01-01-2015', 'DD-MM-YYYY');
 
-  var i = 0;
-  var testDate = moment('01-01-2015', 'DD-MM-YYYY');
-
-  myStages.forEach(function(stage) {
-
-   ['England & Wales', 'Scotland', 'Northern Ireland'].forEach(function(country) {
-
+  myStages.forEach(function (stage) {
+    ['England & Wales', 'Scotland', 'Northern Ireland'].forEach(function (country) {
       for (i = 0; i < maxDatesToTest; i++) {
         testDate.add(1, 'days');
-        var d = new are.Calculator(moment(testDate, 'DD-MM-YYYY'), country, stage.value);
+        d = new are.Calculator(moment(testDate, 'DD-MM-YYYY'), country, stage.value);
 
-        it('[' + d.areDate + '] should not be an weekend: ' + country + ' - ' + stage.label, function() {
-           assert(d.isWeekend(d.areDate) == false);
+        it('[' + d.areDate + '] should not be an weekend: ' + country + ' - ' + stage.label, function () {
+          assert(d.isWeekend(d.areDate) === false);
         });
 
-        it('[' + d.areDate + '] should not be an exclusion day: ' + country + ' - ' + stage.label, function() {
-           assert(d.isExclusionDay(d.areDate) == false);
+        it('[' + d.areDate + '] should not be an exclusion day: ' + country + ' - ' + stage.label, function () {
+          assert(d.isExclusionDay(d.areDate) === false);
         });
-
       }  // over i loop
     });  // over country loop
   });    // over stage loop
