a3e4995124411b8b0b86ec289d6c77862b8766de
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2021-08-22 19:31:27 +0100

    [PIPELINE]: Refactor deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,6 @@ export INGRESS_INTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-internal-annotations.yam
 export INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-external-annotations.yaml
 export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
-export FILEVAULT_NGINX_SETTINGS=$HOF_CONFIG/filevault-nginx-settings.yaml
 
 kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
 
