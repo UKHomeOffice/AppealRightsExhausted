e5dbbcbed727836ca34827bb8c6b9ae0ea56c9e5
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2023-03-02 14:00:32 +0000

    fix linting issue

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, no-shadow */
 
 'use strict';
 
@@ -24,36 +24,35 @@ setInterval(() => {
 // overwrite exclusion_days.json with latest API data and start the application
 const exclusionDates = new ExclusionDates();
 
-  try {
-    exclusionDates.saveExclusionDays();
-  } catch (e) {
-    console.log(e);
-  }
-  
-  const app = hof(settings);
-  
-  app.use((req, res, next) => {
-    res.locals.htmlLang = 'en';
-    // Set feedback link and phase banner
-    res.locals.feedbackUrl = config.survey.urls.root;
+try {
+  exclusionDates.saveExclusionDays();
+} catch (e) {
+  console.log(e);
+}
+const app = hof(settings);
+
+app.use((req, res, next) => {
+  res.locals.htmlLang = 'en';
+  // Set feedback link and phase banner
+  res.locals.feedbackUrl = config.survey.urls.root;
+  next();
+
+  // Set feedback and phase banner on cookies, accessibility and terms pages
+  // along with the getTerms: false, getCookies: false, getAccessibility: false config
+  app.use('/terms-and-conditions', (req, res, next) => {
+    res.locals = Object.assign({}, res.locals, req.translate('terms'));
     next();
-
-    // Set feedback and phase banner on cookies, accessibility and terms pages
-    // along with the getTerms: false, getCookies: false, getAccessibility: false config
-    app.use('/terms-and-conditions', (req, res, next) => {
-      res.locals = Object.assign({}, res.locals, req.translate('terms'));
-      next();
-    });
-
-    app.use('/cookies', (req, res, next) => {
-      res.locals = Object.assign({}, res.locals, req.translate('cookies'));
-      next();
-    });
-
-    app.use('/accessibility', (req, res, next) => {
-      res.locals = Object.assign({}, res.locals, req.translate('accessibility'));
-      next();
-    });
   });
-  
-  module.exports = app;
+
+  app.use('/cookies', (req, res, next) => {
+    res.locals = Object.assign({}, res.locals, req.translate('cookies'));
+    next();
+  });
+
+  app.use('/accessibility', (req, res, next) => {
+    res.locals = Object.assign({}, res.locals, req.translate('accessibility'));
+    next();
+  });
+});
+
+module.exports = app;
