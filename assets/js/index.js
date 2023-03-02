f6b14458cd1821a98da1eff2e445e65b7dcb56be
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2023-03-02 15:11:15 +0000

    remove unused code

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
 
 require('hof/frontend/themes/gov-uk/client-js');
 
-var $ = require('jquery');
+const $ = require('jquery');
 
 if ($('.appeal-summary-class').length) {
   $('.appeal-summary-class').click(function () {
@@ -14,20 +14,17 @@ if ($('.appeal-summary-class').length) {
 if ($('#which-england-and-wales').length) {
   $('#which-england-and-wales').click(function () {
     $('#england-and-wales-dates').toggleClass('js-hidden');
-    $('.old-exclusion-dates-england-and-wales-details').toggleClass('js-hidden');
   });
 }
 
 if ($('#which-scotland').length) {
   $('#which-scotland').click(function () {
     $('#scotland-dates').toggleClass('js-hidden');
-    $('.old-exclusion-dates-scotland-details').toggleClass('js-hidden');
   });
 }
 
 if ($('#which-northern-ireland').length) {
   $('#which-northern-ireland').click(function () {
     $('#northern-ireland-dates').toggleClass('js-hidden');
-    $('.old-exclusion-dates-northern-ireland-details').toggleClass('js-hidden');
   });
 }
