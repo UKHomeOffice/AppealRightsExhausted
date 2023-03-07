90d1ee4f01366380c02ed17739d0b688b8288631
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2023-03-07 15:31:28 +0000

    refactor ui test

--- test/_ui-integration/are_form/exclusiondates.spec.js
+++ test/_ui-integration/are_form/exclusiondates.spec.js
@@ -1,8 +1,19 @@
 const moment = require('moment');
 
+const now = moment();
+const currentYear = now.year();
+const lastYear = moment().year(currentYear - 1).format('YYYY');
+const yearBeforeLastYear = moment().year(currentYear - 2).format('YYYY');
+
 describe('Exclusion dates', () => {
   let testApp;
   let initSession;
+  let tableContent;
+  let tableData;
+  let result;
+  let URI;
+  let res;
+  let docu;
 
   const SUBAPP = '';
 
@@ -13,210 +24,139 @@ describe('Exclusion dates', () => {
     parseHtml = testApp.parseHtml;
   });
 
-  beforeEach(() => {
-    now = moment();
+  beforeEach(async () => {
+    tableContent = '';
+    tableData = '';
+    result = '';
+    URI = '/exclusiondates';
+
+    await initSession(URI);
+    res = await getUrl(URI);
+    docu = await parseHtml(res);
   });
 
   it('goes to exclusion dates', async () => {
-    const URI = '/exclusiondates';
-    await initSession(URI);
-
-
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
     const header = docu.find('header h1');
-
-
     header.html().should.match(/What are the exclusion dates?/);
   });
 
   it('check current year exists in england-and-wales-dates table', async () => {
-    const now = moment();
-    const currentYearToString = now.year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#england-and-wales-dates');
-    const regexCurrentYear = new RegExp(`${currentYearToString}`);
-
-    table.html().should.match(regexCurrentYear);
+    docu.find('#england-and-wales-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
+    const regexCurrentYear = new RegExp(`${currentYear}`);
+    result.should.match(regexCurrentYear);
   });
 
   it('check current year exists in scotland-dates table', async () => {
-    const now = moment();
-    const currentYearToString = now.year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#scotland-dates');
-    const regexCurrentYear = new RegExp(`${currentYearToString}`);
-
-    table.html().should.match(regexCurrentYear);
+    docu.find('#scotland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
+    const regexCurrentYear = new RegExp(`${currentYear}`);
+    result.should.match(regexCurrentYear);
   });
 
   it('check current year exists in northern-ireland-dates table', async () => {
-    const now = moment();
-    const currentYearToString = now.year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#northern-ireland-dates');
-    const regexCurrentYear = new RegExp(`${currentYearToString}`);
-
-    table.html().should.match(regexCurrentYear);
+    docu.find('#northern-ireland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
+    const regexCurrentYear = new RegExp(`${currentYear}`);
+    result.should.match(regexCurrentYear);
   });
 
   it('check last year exists in england-and-wales-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const lastYear = moment().year(currentYear - 1).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#england-and-wales-dates');
+    docu.find('#england-and-wales-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexLastYear = new RegExp(`${lastYear}`);
-
-    table.html().should.match(regexLastYear);
+    result.should.match(regexLastYear);
   });
 
   it('check last year exists in scotland-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const lastYear = moment().year(currentYear - 1).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#scotland-dates');
+    docu.find('#scotland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexLastYear = new RegExp(`${lastYear}`);
-
-    table.html().should.match(regexLastYear);
+    result.should.match(regexLastYear);
   });
 
   it('check last year exists in northern-ireland-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const lastYear = moment().year(currentYear - 1).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#northern-ireland-dates');
+    docu.find('#northern-ireland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexLastYear = new RegExp(`${lastYear}`);
-
-    table.html().should.match(regexLastYear);
+    result.should.match(regexLastYear);
   });
 
   it('check year before last year should NOT exist in england-and-wales-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#england-and-wales-dates');
+    docu.find('#england-and-wales-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.not.match(regexYearBeforeLastYear);
+    result.should.not.match(regexYearBeforeLastYear);
   });
 
   it('check year before last year should NOT exist in scotland-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#scotland-dates');
+    docu.find('#scotland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.not.match(regexYearBeforeLastYear);
+    result.should.not.match(regexYearBeforeLastYear);
   });
 
   it('check year before last year should NOT exist in northern-ireland-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#northern-ireland-dates');
+    docu.find('#northern-ireland-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.not.match(regexYearBeforeLastYear);
+    result.should.not.match(regexYearBeforeLastYear);
   });
 
   it('check year before last year should exist in england-and-wales-old-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#england-and-wales-old-dates');
+    docu.find('#england-and-wales-old-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.match(regexYearBeforeLastYear);
+    result.should.match(regexYearBeforeLastYear);
   });
 
   it('check year before last year should exist in scotland-old-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#scotland-old-dates');
+    docu.find('#scotland-old-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.match(regexYearBeforeLastYear);
+    result.should.match(regexYearBeforeLastYear);
   });
 
   it('check year before last year should exist in northern-ireland-old-dates table', async () => {
-    const now = moment();
-    const currentYear = now.year();
-    const yearBeforeLastYear = moment().year(currentYear - 2).month(1).day(1).year().toString();
-
-    const URI = '/exclusiondates';
-    await initSession(URI);
-    const res = await getUrl(URI);
-    const docu = await parseHtml(res);
-
-    const table = docu.find('#northern-ireland-old-dates');
+    docu.find('#northern-ireland-old-dates tbody .govuk-table__row .govuk-table__cell').each(function (i, el) {
+      tableData = tableContent.concat(' ', el.textContent);
+      result += tableData;
+      return result;
+    });
     const regexYearBeforeLastYear = new RegExp(`${yearBeforeLastYear}`);
-
-    table.html().should.match(regexYearBeforeLastYear);
+    result.should.match(regexYearBeforeLastYear);
   });
 });
