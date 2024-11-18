ee0e1b70d401558c59b13b66339e7ac0f525b81c
William Gu <william.gu@digital.homeoffice.gov.uk>
2024-11-18 12:26:27 +0000

    simple hello world starting page, so we can focus on testing the requests

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
-/* eslint-disable no-undef */
+const express = require('express')
+const app = express()
+const port = 8080
 
-'use strict';
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-const hof = require('hof');
-let settings = require('./hof.settings');
-const ExclusionDates = require('./apps/are_form/models/exclusion_dates');
-const config = require('./config.js');
-
-settings = Object.assign({}, settings, {
-  behaviours: settings.behaviours.map(require),
-  routes: settings.routes.map(require),
-  getCookies: false,
-  getTerms: false,
-  getAccessibility: false,
-  csp: {
-    imgSrc: [
-      'www.google-analytics.com',
-      'ssl.gstatic.com',
-      'www.google.co.uk/ads/ga-audiences'
-    ],
-    connectSrc: [
-      'https://www.google-analytics.com',
-      'https://region1.google-analytics.com',
-      'https://region1.analytics.google.com'
-    ]
-  }
-});
-app.get('/', (req, res) => {  res.send('Hello World!')})
-app.listen(port, () => {  console.log(`Example app listening on port ${port}`)})
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
