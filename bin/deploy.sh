1b33b9cebbc53a1d32885a3355badf655eac42e8
jholderdevops <jonathan.holder1@digital.homeoffice.gov.uk>
2025-02-07 13:24:45 +0000

    hotfix - uat deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -26,7 +26,7 @@ if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/redis -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
-  $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
+  $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml -f kube/app/ingress-external.yml
   $kd -f kube/redis -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
